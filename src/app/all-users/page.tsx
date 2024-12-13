328f4297dc91cd2beaae5ddd331cd2ea50365b41
XyRoHabibi <falbie790@gmail.com>
2024-12-13 17:46:06 +0800

    Merapikan Code

--- src/app/all-users/page.tsx
+++ src/app/all-users/page.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import Header from '../components/Header';
-import AllUsers from '../components/AllUsers';
+import React from "react";
+import Header from "../components/Header";
+import AllUsers from "../components/AllUsers";
 
 function UserPage() {
   return (
@@ -10,11 +10,11 @@ function UserPage() {
       </section>
       <br />
       <div className="container mx-auto py-8">
-      <h1 className="text-2xl text-black font-bold mb-4">All User</h1>
-      <AllUsers />
-    </div>
+        <h1 className="text-2xl text-black font-bold mb-4">All User</h1>
+        <AllUsers />
+      </div>
     </>
-  )
+  );
 }
 
 export default UserPage;
--- src/app/components/Login.tsx
+++ src/app/components/Login.tsx
@@ -5,20 +5,13 @@ import Logo from "@/assets/Pict Inventory.png";
 import Image from "next/image";
 
 function Login() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
   const router = useRouter();
 
   const handleLogin = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    
-    // Contoh validasi sederhana (ganti dengan autentikasi sebenarnya)
-    if (username === "admin" && password === "password") {
-      // Jika login berhasil, pindah ke halaman user
-      router.push('/user');
-    } else {
-      // Tampilkan pesan error
-      alert("Username atau password salah");
+
+    {
+      router.push("/all-users");
     }
   };
 
@@ -26,12 +19,14 @@ function Login() {
     <>
       <div className="flex justify-center items-center ">
         <div className="bg-dark bg-50 grid md:grid-cols-2 grid-cols-1 border rounded-lg shadow-lg max-w-3xl w-full">
-          {/* Logo Section */}
           <div className="flex justify-center items-center p-10">
-            <Image src={Logo} alt="Inventory Logo" className="w-3/4 h-auto object-contain rounded-l-lg" />
+            <Image
+              src={Logo}
+              alt="Inventory Logo"
+              className="w-3/4 h-auto object-contain rounded-l-lg"
+            />
           </div>
 
-          {/* Form Section */}
           <div className="flex flex-col justify-center p-10">
             <form onSubmit={handleLogin}>
               <h1 className="text-gray-800 text-center mb-6 font-bold text-3xl">
@@ -39,19 +34,13 @@ function Login() {
               </h1>
               <input
                 type="text"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
                 className="bg-gray-100 border outline-none rounded-md py-2 px-4 w-full mb-4 text-black"
                 placeholder="Username"
-                required
               />
               <input
                 type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
                 className="bg-gray-100 border outline-none rounded-md py-2 px-4 w-full mb-6 text-black"
                 placeholder="Password"
-                required
               />
               <div className="flex justify-center">
                 <button
@@ -62,7 +51,10 @@ function Login() {
                 </button>
               </div>
               <p className="text-center text-gray-500">
-                New user? <a href="#" className="text-gray-600 hover:underline">Register</a>
+                New user?{" "}
+                <a href="#" className="text-gray-600 hover:underline">
+                  Register
+                </a>
               </p>
             </form>
           </div>
